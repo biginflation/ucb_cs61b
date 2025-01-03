7a55b4da1f4cd8fb0d9b15f8094c8421afdc7558
biginflation <biginflation@outlook.com>
2025-01-04 01:47:16 +0800

    completed first part of lab1

---
+++ lab1/HelloNumbers.java
@@ -0,0 +1,11 @@
+public class HelloNumbers {
+    public static void main(String[] args) {
+        int s = 0;
+        int i = 0;
+        while (s < 45) {
+            s = s + i;
+            System.out.print(s + " ");
+            i = i + 1;
+        }
+    }
+}
